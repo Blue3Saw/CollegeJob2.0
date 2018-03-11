296615759309c5094293c62ea75652459bb4bde6
Blue3Saw <euan.alberto415@gmail.com>
2018-03-11 17:10:36 -0600

    Pinche mapa culero

--- CollegeJob/Controllers/FrontEnd/TareasController.cs
+++ CollegeJob/Controllers/FrontEnd/TareasController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Business_Object;
+using Data_Access_Object;
+using System.Data;
 
 namespace CollegeJob.Controllers
 {
     public class TareasController : Controller
     {
+        TareasDAO ObjTareas = new TareasDAO();
+
         // GET: Tareas
 
         public ActionResult Index()
@@ -28,6 +33,14 @@ namespace CollegeJob.Controllers
 
         public ActionResult AgregarTarea()
         {
+            DataTable Categorias = ObjTareas.categorias();
+            List<string> ListCategorias = new List<string>();
+            foreach (DataRow item in Categorias.Rows)
+            {
+                ListCategorias.Add(item["Clasificacion"].ToString());
+            }
+
+            ViewData["categoria"] = new SelectList(ListCategorias);
             return View();
         }
     }
