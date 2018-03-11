a9ec1c11451fd16732cb4e0801aee5c0126b31cc
Blue3Saw <euan.alberto415@gmail.com>
2018-03-10 19:15:59 -0600

    Agregada plantilla de empleador

--- CollegeJob/Controllers/FrontEnd/TareasController.cs
+++ CollegeJob/Controllers/FrontEnd/TareasController.cs
@@ -25,5 +25,10 @@ namespace CollegeJob.Controllers
         {
             return View();
         }
+
+        public ActionResult AgregarTarea()
+        {
+            return View();
+        }
     }
 }
