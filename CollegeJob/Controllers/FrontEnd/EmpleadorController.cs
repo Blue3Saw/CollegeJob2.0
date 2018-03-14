68dc2bfa20c06495df5b03ca3c30fdced369d6bd
Mikemaple <rejon187@gmail.com>
2018-03-14 07:26:36 -0600

    :v

--- CollegeJob/Controllers/FrontEnd/EmpleadorController.cs
+++ CollegeJob/Controllers/FrontEnd/EmpleadorController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Data_Access_Object;
+using Business_Object;
 
 namespace CollegeJob.Controllers.FrontEnd
 {
     public class EmpleadorController : Controller
     {
         TareasDAO ObjDao = new TareasDAO();
+        MensajesBO bo = new MensajesBO();
+        MensajesDAO objMensajes = new MensajesDAO();
         // GET: Empleador
         public ActionResult Index()
         {
@@ -31,8 +34,22 @@ namespace CollegeJob.Controllers.FrontEnd
 
         public ActionResult Mensajes()
         {
+            return View(objMensajes.MostarMensajes(int.Parse(Session["Codigo"].ToString())));
+        }
 
-            return View();
+        [HttpPost]
+        public ActionResult enviar(string Titulo, string Mensaje, string id)
+        {
+            bo.HoraFecha = DateTime.Now;
+            bo.Mensaje = Mensaje;
+            bo.UsRecibe = int.Parse(id);
+            bo.UsEnvia = int.Parse(Session["Codigo"].ToString());
+            bo.idmensaje = int.Parse(id);
+            bo.Titulo = Titulo;
+            objMensajes.ActualizarEstatus(bo);
+            objMensajes.AgregarMensaje(bo);
+            Mensajes();
+            return View("Mensajes");
         }
     }
 }
