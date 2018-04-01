d9a66e4bdf3695b832ac9f129d6d6a4ee8a1bf3f
Blue3Saw <euan.alberto415@gmail.com>
2018-04-01 16:59:08 -0500

    Cambios de tipo usuario, categorías y agregar nuevo usuario

--- CollegeJob/Controllers/BackEnd/CategoriasYTiposController.cs
+++ CollegeJob/Controllers/BackEnd/CategoriasYTiposController.cs
@@ -13,24 +13,81 @@ namespace CollegeJob.Controllers.BackEnd
         ClasificacionTareaDAO dao = new ClasificacionTareaDAO();
         TipoUsuarioDAO DAO = new TipoUsuarioDAO();
         // GET: CategoriasYTipos
-        public ActionResult Index()
+        public ActionResult Index(string codigo, string tipo)
         {
-            return View();
+            TipoUsuarioDAO tipoUsuarioDAO = new TipoUsuarioDAO();
+            ViewData["TablaTipo"] = tipoUsuarioDAO.VerTipoUs();
+
+            if(codigo == null)
+            {
+                ViewBag.CodigoTipo = Session["CodigoTipo"];
+                ViewBag.TipoUsuario = Session["TipoUsuario"];
+            }
+            else
+            {
+                Session["CodigoTipo"] = codigo;
+                Session["TipoUsuario"] = tipo;
+                ViewBag.CodigoTipo = Session["CodigoTipo"];
+                ViewBag.TipoUsuario = Session["TipoUsuario"];
+            }
+
+            Session["CodigoTipo"] = null;
+            Session["TipoUsuario"] = null;
+            
+            return View("Index");
         }
-        public ActionResult AgregarCategoria()
+
+        public ActionResult AgregarCategoria(string codigo, string clasif)
         {
-            return View();
+            ClasificacionTareaDAO clasificacionTareaDAO = new ClasificacionTareaDAO();
+            ViewData["TablaClasif"] = clasificacionTareaDAO.TablaClasificacion();
+
+            if (codigo == null)
+            {
+                ViewBag.CodigoClasif = Session["CodigoClasif"];
+                ViewBag.Clasificacion = Session["Clasificacion"];
+            }
+            else
+            {
+                Session["CodigoClasif"] = codigo;
+                Session["Clasificacion"] = clasif;
+                ViewBag.CodigoClasif = Session["CodigoClasif"];
+                ViewBag.Clasificacion = Session["Clasificacion"];
+            }
+
+
+            if (ViewBag.Contador == 1)
+            {
+                ViewBag.Contador = 2;
+                ViewBag.CodAgreCat = "Correcto";                
+            }
+            else
+            {
+                ViewBag.CodAgreCat = null;
+                ViewBag.Contador = 0;
+            }
+
+            Session["CodigoClasif"] = null;
+            Session["Clasificacion"] = null;
+
+            return View("AgregarCategoria");
         }
+
         [HttpPost]
         public ActionResult AgregarCate(string categoria)
         {
             ClasificacionTareaBO bo = new ClasificacionTareaBO();
             bo.Clasificacion = categoria;
             int CodAgreCat = dao.AgregarClasificación(bo);
-            Session["CodAgreCat"] = CodAgreCat;
-            ViewBag.CodAgreCat = Session["CodAgreCat"];
-            return View("Index");
+            if(CodAgreCat == 0)
+            {
+                ViewBag.Contador = 1;
+                ViewBag.CodAgreCat = "Correcto";
+            }            
+            AgregarCategoria(null, null);
+            return View("AgregarCategoria");
         }
+
         public ActionResult Agregartipo(string Tipo)
         {
             TipoUsuarioBO bo = new TipoUsuarioBO();
@@ -41,37 +98,29 @@ namespace CollegeJob.Controllers.BackEnd
             return View("AgregarCategoria");
         }
 
-        public ActionResult tablacategorias()
-        {
-            return View(dao.TablaClasificacion());
-        }
-
-        public ActionResult tablausuarios()
-        {
-            return View(DAO.VerTipoUs());
-        }
-
         [HttpPost]
-        public ActionResult TablaActualizarCate(string Tipo, string Tiposelect)
+        public ActionResult ActualizarTipo(string Codigo, string Nuevo)
         {
             TipoUsuarioBO bo = new TipoUsuarioBO();
-            bo.TipoUsuario = Tipo;
-            bo.Codigo = int.Parse(Tiposelect);
+            bo.TipoUsuario = Nuevo;
+            bo.Codigo = int.Parse(Codigo);
             int CodActTip = DAO.ActualizarTipoUsuario(bo);
             Session["CodActTip"] = CodActTip;
             ViewBag.CodActTip = Session["CodActTip"];
-            return View("AgregarCategoria");
+            Index(null, null);
+            return View("Index");
         }
 
         [HttpPost]
-        public ActionResult actcate(string Tipo, string Tiposelect)
+        public ActionResult ActualizarClasif(string Codigo, string Nuevo)
         {
             ClasificacionTareaBO bo = new ClasificacionTareaBO();
-            bo.Clasificacion = Tipo;
-            bo.Codigo = int.Parse(Tiposelect);
+            bo.Clasificacion = Nuevo;
+            bo.Codigo = int.Parse(Codigo);
             int CodActClas = dao.ActualizarClasificaion(bo);
             Session["CodActClas"] = CodActClas;
             ViewBag.CodActClas = Session["CodActClas"];
+            AgregarCategoria(null, null);
             return View("AgregarCategoria");
         }
 
@@ -86,14 +135,14 @@ namespace CollegeJob.Controllers.BackEnd
             return View(DAO.buscarTipo());
         }
 
-        public ActionResult Eliminar(string id)
+        public ActionResult EliminarClasif(string Codigo)
         {
             ClasificacionTareaBO bo = new ClasificacionTareaBO();
-            bo.Codigo = int.Parse(id);
+            bo.Codigo = int.Parse(Codigo);
             int CodElClas = dao.Eliminar(bo);
             Session["CodElClas"] = CodElClas;
             ViewBag.CodElClas = Session["CodElClas"];
-            AgregarCategoria();
+            AgregarCategoria(null, null);
             return View("AgregarCategoria");
         }
     }
